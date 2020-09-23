b09994eec45f30ee0c5fe65b2800da5c2a49a5bd
八亩丘樵夫 <66364850+fptjy@users.noreply.github.com>
2020-09-23 22:38:16 +0800

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+"""
+Exceptions module
+"""
+
+
+class CuckooFilterFullException(Exception):
+    """
+    Exception raised when filter is full.
+    """
+    pass
+
+
+#import os
+#print(os.getcwd())#获取当前工作目录
